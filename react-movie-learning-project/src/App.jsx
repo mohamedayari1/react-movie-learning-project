9753e5d457c8998cbe255a2c0def40407165a2da
mohamedayari1 <ayarim781@gmail.com>
2025-05-18 18:49:29 +0100

    navbar component

--- react-movie-learning-project/src/App.jsx
+++ react-movie-learning-project/src/App.jsx
@@ -3,23 +3,23 @@ import './App.css'
 import MovieCard from "./components/MovieCard"
 import Home from "./pages/Home"
 
+import Favorites from "./pages/Favorites"
+import { Routes, Route } from "react-router-dom"
+import NavBar from "./components/Navbar"
 function App() {
   return (
-    <>
-      {/* <MovieCard movie = {{title : "kachta movie",
-        release_date : "2023",
-        url : "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4x1j0q5g6k7J8X9Y0Z1m2Qz5c4E.jpg"
-      }}/> */}
 
-        <Home />
-      </>
+
+      <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>  
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+      </div>
   )
-}
-
-function Text({inputText}) {
-
-  return <div><p>{inputText}  </p></div>
-
 }
 
 
---
+++ react-movie-learning-project/src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+
+function Favorites() {
+  return (
+    <div className="favorites">
+      <h1>Favorites</h1>
+      <p>Here you can find your favorite movies.</p>
+      <div className="movie-list">
+        {/* Movie cards will be rendered here */}
+      </div>
+    </div>
+  )
+}
+export default Favorites;
